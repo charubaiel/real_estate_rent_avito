f568a43a912ee2a375609a89c00cf7b0ba67c140
Артем Александрин Home <aleksandrinay@it.mos.ru>
2021-11-14 14:42:28 +0300

    initial commit

---
+++ __init__.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import requests as r
+import sqlite3
+from bs4 import BeautifulSoup
+import time
+import numpy as np
+from tqdm import tqdm
+import logging
+import argparse
+
+
+
+def get_item_list(html_data):
+    bs = BeautifulSoup(html_data,features="lxml")
+    return bs.find('div',{'class':'items-items-kAJAg'}).findAll('div',{'data-marker':'item'})
+
+def get_item_info(item):
+    item_desc = {}
+    try:
+        item_desc['date'] = pd.to_datetime('today').date()
+        item_desc['publish_delta'] = item.find('div',{'data-marker':'item-date'}).text
+        item_desc['id'] = item['id']
+        item_desc['url'] = item.a['href']
+        item_desc['title'] = item.a['title']
+        item_desc['text'] = item.meta['content']
+        item_desc['price'] = item.find('meta',{'itemprop':'price'})['content']
+        item_desc['adress'] = item.find('div',{'data-marker':'item-address'}).span.text
+        item_desc['metro'] = item.find('div',{'data-marker':'item-address'}).div.text.replace(item_desc['adress'],'').replace('\xa0',' ')
+    except:
+        pass
+
+    return item_desc
+
+def save_to_db(data,table_name,db_path):
+    with sqlite3.connect(db_path) as w:
+        pd.DataFrame(data).to_sql(name = table_name,con=w,if_exists='append')
+
+
+def fix_data(data):
+    df = data.copy()
+    df['price'] = df['price'].astype(float)
+    # df['home_num']=df['adress'].str.extract(', (\d.*)')
+    df['street']=df['adress'].str.extract('(.*?), (?=\d.*)')
+    df[['stantion','metro_distance']] = df['metro'].str.split(',',expand=True,n=1)
+    df['metro_distance'] = df['metro_distance'].str.strip().str.split(' ',expand=True)[0].str.replace(',','.').astype(float).apply(lambda x: x*1000 if x<100 else x)
+    df['n_rooms']=df['title'].str.extract('«(.*?),')
+    df['m2'] = df['title'].str.extract(', (\d+).*м²').astype(float)
+    df[['floor','max_floor']] = df['title'].str.extract('(\d+/\d+).*эт')[0].str.split('/',expand=True)
+    df['text'] = df['text'].str.replace('\n','')
+    df['rubm2'] = df['price'] / df['m2']
+    df['rubm2m'] = df['rubm2'] * df['metro_distance'] / 1000
+    df.drop(['title','adress','metro'],axis=1,inplace=True)
+    return df
---
+++ parse.py
@@ -0,0 +1,66 @@
+from __init__ import *
+
+
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-rent','--is_rent',action="store_true",
+                    help="scan rent instead of real estate")
+args = parser.parse_args()
+
+
+
+
+DB_PATH = r"data\avito_real_estate.db"
+
+TABLE_NAME = "moscow_RE"
+START_URL_PAGE = 'https://www.avito.ru/moskva/kvartiry/prodam/vtorichka-ASgBAQICAUSSA8YQAUDmBxSMUg?cd=1&f=ASgBAQICAUSSA8YQBkDmBxSMUsoIJIRZglnmFhTm_AGQvg0Ulq41rL4NFKTHNcDBDRS6_Tc&p=1'
+
+if args.is_rent:
+    
+    TABLE_NAME = "moscow_rent"
+    START_URL_PAGE = 'https://www.avito.ru/moskva/kvartiry/sdam/na_dlitelnyy_srok-ASgBAgICAkSSA8gQ8AeQUg?cd=1&f=ASgBAQICAkSSA8gQ8AeQUgNAzAgkjFmOWegWFOj8Aay~DRSkxzU&p=1'
+
+
+logging.basicConfig(level=logging.INFO,filename=rf'data\{TABLE_NAME}.log',format="%(asctime)s - %(levelname)s - %(name)s - %(message)s")
+
+
+if __name__ == '__main__':
+
+    logging.info('start_scan')
+
+    start_page_html = r.get(START_URL_PAGE)
+
+    start_page = BeautifulSoup(start_page_html.text,features="lxml")
+
+    max_pages = int(start_page.find('div',{'data-marker':'pagination-button'}).findAll('span')[-2].text)
+
+    logging.info(f'max_pages find : {max_pages}')
+
+    err_count = 0
+
+    for i in tqdm(range(1,max_pages)):
+        try:
+            rng = np.random.poisson(5)
+            time.sleep(rng)
+            
+            qq = r.get(START_URL_PAGE.replace('&p=1',f'&p={i}'))
+            
+            item_list = get_item_list(qq.text)
+            
+            page_df = [get_item_info(i) for i in item_list]
+            
+
+            save_to_db(pd.DataFrame(page_df),table_name=TABLE_NAME,db_path=DB_PATH)
+
+        except:
+            err_count+=1
+            logging.error(f'error on {i}th page')
+
+            if err_count ==3 :
+                break
+
+
+
+    logging.info(f'end of scann')
