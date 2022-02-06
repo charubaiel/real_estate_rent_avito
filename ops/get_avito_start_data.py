fd9c29a456ce1c2fd1d95efa6454cea533c06041
Charubaiel <aleksandrin.a@mail.ru>
2022-02-06 17:21:34 +0300

    add poisson lag

--- ops/get_avito_start_data.py
+++ ops/get_avito_start_data.py
@@ -1,5 +1,7 @@
 from dagster import op ,get_dagster_logger as log
 import requests as r
+import time
+import numpy as np
 
 
 
@@ -8,7 +10,7 @@ def get_avito_page1(url):
     
     headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36 Edg/97.0.1072.62',
         'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'}
-
+    time.sleep(np.random.poisson(15))
     start_page_html = r.get(url,headers=headers)
 
     if start_page_html.status_code == 200:
@@ -24,7 +26,7 @@ def get_avito_page2(url):
     
     headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36 Edg/97.0.1072.62',
         'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'}
-
+    time.sleep(np.random.poisson(15))
     start_page_html = r.get(url,headers=headers)
 
     if start_page_html.status_code == 200:
@@ -40,7 +42,7 @@ def get_avito_page3(url):
     
     headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36 Edg/97.0.1072.62',
         'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'}
-
+    time.sleep(np.random.poisson(15))
     start_page_html = r.get(url,headers=headers)
 
     if start_page_html.status_code == 200:
@@ -54,7 +56,7 @@ def get_avito_page4(url):
     
     headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36 Edg/97.0.1072.62',
         'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'}
-
+    time.sleep(np.random.poisson(15))
     start_page_html = r.get(url,headers=headers)
 
     if start_page_html.status_code == 200:
@@ -68,7 +70,7 @@ def get_avito_page5(url):
     
     headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36 Edg/97.0.1072.62',
         'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'}
-
+    time.sleep(np.random.poisson(15))
     start_page_html = r.get(url,headers=headers)
 
     if start_page_html.status_code == 200:
